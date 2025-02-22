8436e591bee9cb3ca8d31edc4b0835575efbb212
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-02-22 15:27:34 +0530

    update customer crud operation

--- src/main/java/lk/ijse/pos/controller/MainController.java
+++ src/main/java/lk/ijse/pos/controller/MainController.java
@@ -1,4 +1,54 @@
 package lk.ijse.pos.controller;
 
-public class MainController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private AnchorPane content;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        navigateTo("/view/CustomerView.fxml");
+    }
+
+    @FXML
+    void navigateToCustomerPage(ActionEvent event) {
+        navigateTo("/view/CustomerView.fxml");
+    }
+
+    @FXML
+    void navigateToItemPage(ActionEvent event) {
+        navigateTo("/view/ItemView.fxml");
+    }
+
+    @FXML
+    void navigateToOrdersPage(ActionEvent event) {
+        navigateTo("/view/OrdersView.fxml");
+    }
+
+    public void navigateTo(String fxmlPath) {
+        try {
+            content.getChildren().clear();
+            AnchorPane load = FXMLLoader.load(getClass().getResource(fxmlPath));
+
+            load.prefWidthProperty().bind(content.widthProperty());
+            load.prefHeightProperty().bind(content.heightProperty());
+
+
+            content.getChildren().add(load);
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Fail to load page!").show();
+        }
+    }
 }
